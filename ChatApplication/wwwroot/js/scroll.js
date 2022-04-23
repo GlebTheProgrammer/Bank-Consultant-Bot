3fb8be2017469c97d6fc0561707cf163f845909d
Gleb Sukristik <gleb15a@gmail.com>
2022-04-23 13:16:35 +0300

    Added custom scroll bar. Added border to all user messages. Changed some colors and added some other features

---
+++ ChatApplication/wwwroot/js/scroll.js
@@ -0,0 +1,6 @@
+﻿var targetDiv = document.querySelector('.chat-box');
+targetDiv.scrollTop = targetDiv.scrollHeight;
+
+//$(document).ready(function () {
+//    $('.chat-box').scrollTop( $('.chat-box')[0].scrollHeigh );
+//});
