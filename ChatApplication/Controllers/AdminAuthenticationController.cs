e7bee4adf3bc01d92af480157224dd7d60cddffc
Gleb Sukristik <gleb15a@gmail.com>
2022-04-24 12:37:48 +0300

    Uploaded new project for identity server. Added some new view for admin for the future

---
+++ ChatApplication/Controllers/AdminAuthenticationController.cs
@@ -0,0 +1,18 @@
+﻿using ChatApplication.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApplication.Controllers
+{
+    public class AdminAuthenticationController : Controller
+    {
+        private readonly IChatRepository repository;
+        public AdminAuthenticationController(IChatRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
