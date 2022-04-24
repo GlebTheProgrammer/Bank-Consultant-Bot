149888ed3468f29b9086ed981e8325519c357268
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-04-24 17:40:35 +0300

    Changed location of bot api, and, consequently, changed host of api in appsettings.json. Init identity server project as web api.

---
+++ OtoBotAPI/Controllers/BotController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtoBotAPI.Controllers;
+
+/// <summary>
+/// Point of bot answer for you request.
+/// </summary>
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+public class BotController : ControllerBase
+{
+
+    private readonly ILogger<BotController> _logger;
+
+    public BotController(ILogger<BotController> logger)
+    {
+            _logger = logger;
+    }
+
+    /// <summary>
+    /// sends answer.
+    /// </summary>
+    /// <returns>String answer.</returns>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        return "Hello from bot.";
+    }
+        
+    
+}
